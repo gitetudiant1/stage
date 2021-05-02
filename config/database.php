9cb0ba0daf67ccac851c77390c9ee71b09962f3f
agnes kintzler <agnes.kintzler@ac-amiens.fr>
2021-05-02 14:45:46 +0200

    Création initiale de l'arborescence

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+$configDatabase = [
+    'dbname'  => 'nombd',
+    'user'    => 'utilisateur',
+    'pwd'     => 'votre@mot@de@passe',
+    'host'    => 'localhost',
+    'port'    => '3307',
+    'charset' => 'UTF8',
+];
---
+++ model/StageManager.php
@@ -0,0 +1,7 @@
+<?php
+require_once("model/Manager.php"); 
+
+class StageManager extends Manager
+{
+    // ...
+}
